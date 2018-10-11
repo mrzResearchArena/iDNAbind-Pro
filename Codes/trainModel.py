99fca287e3bb4db1b211d777d820cd94c2b4b13e
mrzResearchArena <mrz30792@gmail.com>
2018-10-12 03:40:02 +0600

    iDNAbind-Pro

---
+++ Codes/trainModel.py
@@ -0,0 +1,157 @@
+### Avoid warning ###
+import warnings
+def warn(*args, **kwargs): pass
+warnings.warn = warn
+
+
+### Essential ###
+import pandas as pd
+import numpy as np
+
+
+### Fixed State ###
+# np.random.seed(seed=111)
+
+# scikit-learn :
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import BaggingClassifier, \
+    RandomForestClassifier, \
+    AdaBoostClassifier, \
+    GradientBoostingClassifier, \
+    ExtraTreesClassifier
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+
+def train(args):
+
+    ### Load Dataset ###
+    D = pd.read_csv(args.dataset, header=None)
+
+    ### Splitting dataset into X, Y ###
+    X_train = D.iloc[:, :-1].values
+    Y_train = D.iloc[:, -1].values
+
+
+    ### Random Shuffle ###
+    from sklearn.utils import shuffle
+    X_train, Y_train = shuffle(X_train, Y_train)  # Avoiding bias
+
+
+    ## Scaling using sci-kit learn ###
+    from sklearn.preprocessing import StandardScaler
+    scale = StandardScaler()
+    X_train = scale.fit_transform(X_train)
+
+
+    ### Run Machine Learning Model ###
+    print('Model training using ', end='')
+
+    #1. using LR:
+    if args.model == 'LR':
+        model = LogisticRegression()
+        print('LR ', end='')
+
+    #2. using KNN:
+    if args.model == 'KNN':
+        model = KNeighborsClassifier(n_neighbors=args.K)
+        print('KNN ', end='')
+
+    #3. using DT:
+    if args.model == 'DT':
+        model = DecisionTreeClassifier()
+        print('DT ', end='')
+
+    #4. using SVM:
+    if args.model == 'SVM':
+        model = SVC(probability=True)
+        print('SVM ', end='')
+
+
+    #5. using NB:
+    if args.model == 'NB':
+        model = GaussianNB()
+        print('NB ',end='')
+
+    #6. using Bagging:
+    if args.model == 'Bagging':
+        model = BaggingClassifier()
+        print('Bagging ', end='')
+
+    #7. using RF:
+    if args.model == 'RF':
+        model = RandomForestClassifier()
+        print('RF ', end='')
+
+    #8. using AB:
+    if args.model == 'AB':
+        model = AdaBoostClassifier()
+        print('AB ' , end='')
+
+    #9. using GB:
+    if args.model == 'GB':
+        model = GradientBoostingClassifier()
+        print('GB ', end='')
+
+    #10. using LDA:
+    if args.model == 'LDA':
+        model = LinearDiscriminantAnalysis()
+        print('LDA ', end='')
+
+    '''
+    You can use any model instead of logistics regression,
+    and an even different parameter.
+    
+    Suggested: GridSearchCV
+    (http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html)
+    '''
+
+    ### Fitting Model ###
+    model.fit(X_train, Y_train)
+    ### --- ###
+
+
+    ### Dump Model with joblib ###
+    from sklearn.externals import joblib
+    with open('dumpModel.pkl', 'wb') as File:
+        joblib.dump(model, File)
+
+
+    ### Dump Model with pickle ###
+    # import pickle
+    # with open('modelDump.pkl', 'wb') as pickleFile:
+    #     pickle.dump(model, pickleFile)
+    ##############################
+
+    print('classifier.')
+
+
+import argparse
+
+p = argparse.ArgumentParser(description='Training the model with LR.')
+p.add_argument('-data', '--dataset', type=str, help='~/dataset.csv', default='optimumDataset.csv')
+
+# p.add_argument('-lr', '--LR', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-svm', '--SVM', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+#
+# p.add_argument('-knn', '--KNN', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+p.add_argument('-k', '--K', type=int, help='value of k for KNN', default=5)
+
+# p.add_argument('-dt', '--DT', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-nb', '--NB', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-bag', '--Bagging', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-rf', '--RF', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-ab', '--AB', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-gb', '--GB', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+# p.add_argument('-lda', '--LDA', type=int, help='using Logistics Regression', default=0, choices=[0, 1])
+
+p.add_argument('-m', '--model', type=str, help='choose a model', default='LR', choices=['LR','SVM','KNN','DT','SVM','NB','Bagging','RF','AB','GB','LDA',])
+
+args = p.parse_args()
+
+train(args)
---
+++ RESULTS.md
@@ -0,0 +1,37 @@
+# Performance Measures on Protein Datasets
+
+### DNA-binding Protein, trained on PDB1075 dataset
+| Classifier | Accuracy (%) |  auROC | auPR   | Sn (%) |  Sp (%)  | F1-Score |  MCC   |
+| :---       | :---:        | :---:  |  :---: | :---:  | :---:    | :---:    | ---:  |
+|LR| 90.88 |0.9662|0.9717|91.36|90.42|0.9089|0.8186|
+|SVM|89.67|0.9665|0.9714|90.69|88.80|0.8983|0.7967|
+|KNN| 84.19 |0.9042|0.8778|84.48|83.94|0.8425|0.6851|
+|Decision Tree|75.07|0.7509|0.6883|76.11|74.09|0.7527|0.5039|
+|Naive Bayes|83.72|0.9031|0.8691|83.13|84.35|0.8362|0.6756|
+|Bagging|82.70|0.8941|0.8755|80.70|84.75|0.8233|0.6569|
+|Random Forest|80.65|0.8871|0.8619|76.92|84.35|0.7988|0.6160|
+|AdaBoost|82.88|0.9031|0.9006|83.13|82.59|0.8291|0.6578|
+|Gradient Boosting|85.30|0.9309|0.9338|86.77|83.94|0.8555|0.7096|
+|LDA|87.63|0.9503|0.9563|88.26|87.04|0.8774|0.7541|
+
+Note: The number of DNA-binding protein (+ve) sequences and the number of non-DNA-binding protein (-ve) sequences are 525 and 550 respectively for the PDB1075 dataset.
+
+&nbsp;
+
+### DNA-binding Protein, tested on PDB186 dataset
+| Classifier | Accuracy (%) |  auROC | auPR   | Sn (%) |  Sp (%)  | F1-Score |  MCC   |
+| :---       | :---:        | :---:  |  :---: | :---:  | :---:    | :---:    | :---:  |
+|LR|83.33|0.8780|0.8357|71.93|89.36|0.7480|0.6259|
+|SVM| 81.18 |0.9021|0.7921|70.58|86.57|0.7205|0.5783|
+|KNN| 77.42 |0.8270|0.6715|68.29|81.79|0.6734|0.4990|
+|Decision Tree |70.43|0.6722|0.4768|56.95|77.50|0.5706|0.3457|
+|Naive Bayes |74.73|0.8208|0.6438|73.95|76.29|0.6769|0.4874|
+|Bagging|76.34|0.8206|0.6837|53.04|88.21|0.6052|0.4480|
+|Random Forest|75.27|0.8120|0.6699|49.80|88.36|0.5788|0.4211|
+|AdaBoost|76.34|0.8294|0.7134|63.43|83.14|0.6496|0.4742|
+|Gradient Boosting|78.49|0.8661|0.7706|67.21|86.71|0.6987|0.5519|
+|LDA|79.57|0.8762|0.7817|71.93|86.93|0.7312|0.5937|
+
+Note: The number of DNA-binding protein (+ve) sequences and the number of non-DNA-binding protein (-ve) sequences are 93 and  93 respectively for the PDB186 dataset.
+
+We can [download](http://server.malab.cn/Local-DPP/Datasets.html) DNA-binding protein datasets (PDB1075 and PDB186) from given link.
